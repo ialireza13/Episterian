e0206e3d1240dc4420482038bc6938e584711f7c
sepante <sinasajjadi@protonmail.com>
2020-03-29 16:54:34 +0430

    flux function added. agent infected by person to person and environment to person are not distinguished. algorithm now supports more multiple infetious agents.

--- simulate.py
+++ simulate.py
@@ -42,18 +42,46 @@ def simulate(args):
     def get_infetced(agents, pollution):
         susceptibles = agents['health'] == 0
         susceptibles_num = np.sum(susceptibles)
+        #from environment infection
+        from_env_inf = np.random.random( susceptibles_num ) < pollution[ agents['tile_x'][susceptibles], agents['tile_y'][susceptibles] ]
+        agents['health'][susceptibles] = from_env_inf
+        from_env_num = np.sum(from_env_inf)
 
-        agents['health'][susceptibles] = np.random.random( susceptibles_num ) < pollution[ agents['tile_x'][susceptibles], agents['tile_y'][susceptibles] ]
+        #from person infection
+#         tx = agents[agents['health']==2]['tile_x'][0]
+#         ty = agents[agents['health']==2]['tile_y'][0]
+#         on_tile = [all([agent['tile_x']==tx, agent['tile_y']==ty, agent['health']==0]) for agent in agents]
 
-        tx = agents[agents['health']==2]['tile_x'][0]
-        ty = agents[agents['health']==2]['tile_y'][0]
-        on_tile = [all([agent['tile_x']==tx, agent['tile_y']==ty, agent['health']==0]) for agent in agents]
+        infectors = agents[ agents['health'] == 2 ]
+
+        on_tile = []
+        for infector in infectors:
+            tx = infector['tile_x']
+            ty = infector['tile_y']
+            on_tile.extend([all([agent['tile_x']==tx, agent['tile_y']==ty, agent['health']==0]) for agent in agents])
+
+
+
         on_tile_num = np.sum(on_tile)
-        agents['health'][on_tile] = np.random.random(on_tile_num) < infection_rate
-
-        return susceptibles_num
         
-    disease_timeline = np.zeros( tMax ,dtype="int" )
+        from_per_inf = np.random.random(on_tile_num) < infection_rate
+        agents['health'][on_tile] = from_per_inf
+        from_per_num = np.sum(from_per_inf)
+
+        return from_per_num, from_env_num
+    
+    def flow(agent, init_infection_prob):
+        leaver = np.random.randint(len( agents ))
+#         agents[leaver]['health'] = not(np.random.random() < init_infection_prob)
+#         agents[leaver]['health'] *= 2
+        agents[leaver]['health'] = np.random.choice( [0,2], p=[1-init_infection_prob, init_infection_prob] )
+        agents[leaver]['x'] = np.random.random() * Lx
+        agents[leaver]['y'] = np.random.random() * Ly
+
+        
+    #disease_timeline = np.zeros( tMax ,dtype="int" )
+    disease_timeline = np.zeros( ( tMax ), dtype=[ ('from_per',int), ('from_env',int)] )
+
     agents = np.zeros((N), dtype=[('x', 'float'), ('y', 'float'), ('tile_x',int), ('tile_y',int), ('health',int)] )
     agents['x'] = np.random.random(N) * Lx
     agents['y'] = np.random.random(N) * Ly
@@ -67,5 +95,7 @@ def simulate(args):
         walk(agents)
         update_tile(agents)
         pollute(agents, pollution)
-        disease_timeline[t] = N - get_infetced(agents, pollution)
+        disease_timeline[t]['from_per'], disease_timeline[t]['from_env'] = get_infetced(agents, pollution)
+        
+        
     return disease_timeline
